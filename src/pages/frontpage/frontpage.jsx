529fa78197543710d512480958ec3eb556110fa3
Oliver Juul <Oliverjuul9@gmail.com>
2021-07-20 20:24:39 +0200

    .

--- src/pages/frontpage/frontpage.jsx
+++ src/pages/frontpage/frontpage.jsx
@@ -1,19 +1,19 @@
-import React from 'react' 
+import React from 'react'
 import './frontpage.scss'
 import image from '../../images/IMG_0840.jpg'
-export function FrontPage () {
+export function FrontPage() {
     return (
         <article className="intro-section" id="front-nav">
-                <img src={image} alt="me"/>
-                <div>
-                    <h3>Intro</h3>
-                    <p>Hey, my name is Oliver Juul, I'm 22 years old, and currently studying web development at Aalborg Techcollege.
-                        This page will act as a Portfolio/CV. 
-                        <br/>
-                        <br/>
-                        The page is subject to change shortly in the future 
-                    </p>
-                </div>
+            <img src={image} alt="me" />
+            <div>
+                <h3>Intro</h3>
+                <p>Hey, my name is Oliver Juul, I'm 22 years old, and currently studying web development at Aalborg Techcollege.
+                    This page will act as a Portfolio/CV.
+                    <br />
+                    <br />
+                    For this portfolio I'm using React. Which is a framework built on javascript. I will try my best to update this page as much as i can to show my growing knowledge.
+                </p>
+            </div>
         </article>
     )
 }
--- src/pages/projects/projectpage.jsx
+++ src/pages/projects/projectpage.jsx
@@ -1,27 +1,29 @@
-import React from 'react' 
+import React from 'react'
 import "./projects.scss";
 import urbanimg from '../../images/urban.PNG'
 import image from '../../images/article.jpg'
 
-export function ProjectPage () {
+export function ProjectPage() {
     return (
         <div>
             <section className="project-section" id="project-nav">
                 <h2>Projects</h2>
                 <article>
-                    <div className="project-1"> 
+                    <div className="project-1">
                         <img src={image} alt="Project 1" />
                         <div className="project-info">
-                        <h3>Bakery</h3>
-                        <p>One of the projects we had on react this year was for a bakery. It was the first time we did a full scale project in React App.</p>
+                            <h3>Welder App</h3>
+                            <p>Currently I'm working on a personalized app for the welders on the school. It's being build using Ionic framework, which uses typescript.</p>
+                            <a href="#">Click here to see the project</a>
                         </div>
                     </div>
-                    <div className="project-2"> 
+                    <div className="project-2">
                         <div className="project-info">
-                        <h3>Urban Explorer</h3>
-                        <p>Urban Explorer was a group project we had that involved making a progressive webapp. My group used Ionic framework to make the app.</p> 
+                            <h3>Urban Explorer</h3>
+                            <p>Urban Explorer was a group project we had that involved making a progressive webapp. My group used Ionic framework to make the app.</p>
+                            <a href="#">Click here to see the project</a>
                         </div>
-                        <img src={urbanimg} alt="Project 2"/>
+                        <img src={urbanimg} alt="Project 2" />
                     </div>
                 </article>
             </section>
